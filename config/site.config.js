788e38e11e2e3b6cda9ef99be0e1450de6671c8f
nightrelax <1840688+nightrelax@users.noreply.github.com>
2024-02-14 22:04:11 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,7 +20,7 @@ module.exports = {
   title: "Oliver's CloudDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Auto365 HP/.App man android/' || '/Auto365 HP/.Lắp đặt/',
+  baseDirectory: '/Auto365 HP/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
