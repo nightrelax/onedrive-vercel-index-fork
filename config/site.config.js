bd133e034c1de488cbbb3df983a54e4ebb7d5cfd
nightrelax <1840688+nightrelax@users.noreply.github.com>
2024-04-09 14:21:56 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/DATA', '/Setup', '/Tinh nang an', '/VPS Sync', '/.Lắp đặt'],
+  protectedRoutes: ['/DATA', '/Setup', '/Tinh nang an', '/VPS Sync', '/.Lắp đặt','.App man android'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:linholiver818@gmail.com',
